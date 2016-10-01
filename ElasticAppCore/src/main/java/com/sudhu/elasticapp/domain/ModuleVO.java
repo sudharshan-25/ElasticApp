5ad893d9fe8272c7d89d5a9985a87ed06019c04a
Sudharshan <sudharshan25@hotmail.com>
2016-10-01 18:04:17 +0530

    Initial code setup

---
+++ ElasticAppCore/src/main/java/com/sudhu/elasticapp/domain/ModuleVO.java
@@ -0,0 +1,68 @@
+package com.sudhu.elasticapp.domain;
+
+/**
+ * Created by sudha on 01-Oct-16.
+ */
+public class ModuleVO {
+
+    private String moduleName;
+
+    private String dataBaseName;
+
+    private String dbServerName;
+
+    private String dbConnectionURL;
+
+    private String dbUserName;
+
+    private String dbPassword;
+
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public String getDataBaseName() {
+        return dataBaseName;
+    }
+
+    public void setDataBaseName(String dataBaseName) {
+        this.dataBaseName = dataBaseName;
+    }
+
+    public String getDbServerName() {
+        return dbServerName;
+    }
+
+    public void setDbServerName(String dbServerName) {
+        this.dbServerName = dbServerName;
+    }
+
+    public String getDbConnectionURL() {
+        return dbConnectionURL;
+    }
+
+    public void setDbConnectionURL(String dbConnectionURL) {
+        this.dbConnectionURL = dbConnectionURL;
+    }
+
+    public String getDbUserName() {
+        return dbUserName;
+    }
+
+    public void setDbUserName(String dbUserName) {
+        this.dbUserName = dbUserName;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+}
