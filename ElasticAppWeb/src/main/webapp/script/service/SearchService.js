2ee8fd1f210b2fc5d9cdfd5b9f8f6e963a3549e1
Sudharshan Srinivasan <sudharshan25@hotmail.com>
2016-12-09 19:48:40 +0530

    Encoding the app token

--- ElasticAppWeb/src/main/webapp/script/service/SearchService.js
+++ ElasticAppWeb/src/main/webapp/script/service/SearchService.js
@@ -17,13 +17,13 @@ elasticApp.factory('SearchService', [ '$http', '$q', function($http, $q) {
 	function simpleSearch(simpleSearchCriteria) {
 		var deferred = $q.defer();
 		var data = '?';
-		data += "appToken="+simpleSearchCriteria.appToken;
-		data += "&term="+simpleSearchCriteria.term;
+		data += "appToken="+escape(simpleSearchCriteria.appToken);
+		data += "&term="+escape(simpleSearchCriteria.term);
 		if(simpleSearchCriteria.fields){
-			data += "&fields="+simpleSearchCriteria.fields;
+			data += "&fields="+escape(simpleSearchCriteria.fields);
 		}
 		if(simpleSearchCriteria.wildcard){
-			data += "&wildcard="+simpleSearchCriteria.wildcard;
+			data += "&wildcard="+escape(simpleSearchCriteria.wildcard);
 		}
 		$http.get(REST_SERVICE_URI + 'simpleSearch'+data).then(function(response) {
 			deferred.resolve(response.data);
@@ -36,8 +36,11 @@ elasticApp.factory('SearchService', [ '$http', '$q', function($http, $q) {
 
 	function advancedSearch(searchCriteria) {
 		var deferred = $q.defer();
+		var url = searchCriteria.appToken; 
+		searchCriteria.appToken = escape(searchCriteria.appToken); 
 		$http.post( REST_SERVICE_URI + 'advancedSearch',searchCriteria).then(function(response) {
 			deferred.resolve(response.data);
+			searchCriteria.appToken = url;
 		}, function(errResponse) {
 			console.error('Error while fetching advanced Search');
 			deferred.reject(errResponse);
